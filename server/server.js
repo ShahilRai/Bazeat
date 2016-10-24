587665ab89a29798cd2405185412c82860444dbc
Wasim Akhtar <wasim.akhtar@ongraph.com>
2016-10-24 15:19:11 +0530

    changes in server js file

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ import compression from 'compression';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path';
-import IntlWrapper from '../client/modules/Intl/IntlWrapper';
+// import IntlWrapper from '../client/modules/Intl/IntlWrapper';
 // import twilio from 'twilio';
 import nodeMailer from 'nodemailer';
 import mandrillTransport from 'nodemailer-mandrill-transport';
 
 // Webpack Requirements
 import webpack from 'webpack';
-import config from '../webpack.config.dev';
+import config from '../webpack.config';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
@@ -21,11 +21,15 @@ const app = new Express();
 if (process.env.NODE_ENV === 'development') {
   const compiler = webpack(config);
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
-  app.use(webpackHotMiddleware(compiler));
+  // app.use(webpackHotMiddleware(compiler));
 }
 
+console.log(__dirname)
+app.use('/css', Express.static(path.resolve(__dirname, '../client/css')));
+// app.use('/fonts', express.static(__dirname + '/client/fonts'));
+app.use('/javascript', Express.static(path.resolve(__dirname, '../client/javascript')));
 // React And Redux Setup
-import { configureStore } from '../client/store';
+// import { configureStore } from '../client/store';
 import { Provider } from 'react-redux';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
@@ -33,7 +37,7 @@ import { match, RouterContext } from 'react-router';
 import Helmet from 'react-helmet';
 
 // Import required modules
-import routes from '../client/routes';
+// import routes from '../client/routes';
 import { fetchComponentData } from './util/fetchData';
 import posts from './routes/post.routes';
 import users from './routes/user.routes';
@@ -124,86 +128,90 @@ app.on('ExpressStrompath.ready', () => {
 
 
 // Render Initial HTML
-const renderFullPage = (html, initialState) => {
-  const head = Helmet.rewind();
+// const renderFullPage = (html, initialState) => {
+//   const head = Helmet.rewind();
 
-  // Import Manifests
-  const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
-  const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
+//   // Import Manifests
+//   const assetsManifest = process.env.webpackAssets && JSON.parse(process.env.webpackAssets);
+//   const chunkManifest = process.env.webpackChunkAssets && JSON.parse(process.env.webpackChunkAssets);
 
-  return `
-    <!doctype html>
-    <html>
-      <head>
-        ${head.base.toString()}
-        ${head.title.toString()}
-        ${head.meta.toString()}
-        ${head.link.toString()}
-        ${head.script.toString()}
+//   return `
+//     <!doctype html>
+//     <html>
+//       <head>
+//         ${head.base.toString()}
+//         ${head.title.toString()}
+//         ${head.meta.toString()}
+//         ${head.link.toString()}
+//         ${head.script.toString()}
 
-        ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
-        <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
-      </head>
-      <body>
-        <div id="root">${html}</div>
-        <script>
-          window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
-          ${process.env.NODE_ENV === 'production' ?
-          `//<![CDATA[
-          window.webpackManifest = ${JSON.stringify(chunkManifest)};
-          //]]>` : ''}
-        </script>
-        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
-        <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
-      </body>
-    </html>
-  `;
-};
+//         ${process.env.NODE_ENV === 'production' ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
+//         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
+//         <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+//       </head>
+//       <body>
+//         <div id="root">${html}</div>
+//         <script>
+//           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
+//           ${process.env.NODE_ENV === 'production' ?
+//           `//<![CDATA[
+//           window.webpackManifest = ${JSON.stringify(chunkManifest)};
+//           //]]>` : ''}
+//         </script>
+//         <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/vendor.js'] : '/vendor.js'}'></script>
+//         <script src='${process.env.NODE_ENV === 'production' ? assetsManifest['/app.js'] : '/app.js'}'></script>
+//       </body>
+//     </html>
+//   `;
+// };
 
-const renderError = err => {
-  const softTab = '&#32;&#32;&#32;&#32;';
-  const errTrace = process.env.NODE_ENV !== 'production' ?
-    `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
-  return renderFullPage(`Server Error${errTrace}`, {});
-};
+// const renderError = err => {
+//   const softTab = '&#32;&#32;&#32;&#32;';
+//   const errTrace = process.env.NODE_ENV !== 'production' ?
+//     `:<br><br><pre style="color:red">${softTab}${err.stack.replace(/\n/g, `<br>${softTab}`)}</pre>` : '';
+//   return renderFullPage(`Server Error${errTrace}`, {});
+// };
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/html/index.html'));
+});
 
 // Server Side Rendering based on routes matched by React-router.
-app.use((req, res, next) => {
-  match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
-    if (err) {
-      return res.status(500).end(renderError(err));
-    }
+// app.use((req, res, next) => {
+//   match({ routes, location: req.url }, (err, redirectLocation, renderProps) => {
+//     if (err) {
+//       return res.status(500).end(renderError(err));
+//     }
 
-    if (redirectLocation) {
-      return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
-    }
+//     if (redirectLocation) {
+//       return res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+//     }
 
-    if (!renderProps) {
-      return next();
-    }
+//     if (!renderProps) {
+//       return next();
+//     }
 
-    const store = configureStore();
+//     const store = configureStore();
 
-    return fetchComponentData(store, renderProps.components, renderProps.params)
-      .then(() => {
-        const initialView = renderToString(
-          <Provider store={store}>
-            <IntlWrapper>
-              <RouterContext {...renderProps} />
-            </IntlWrapper>
-          </Provider>
-        );
-        const finalState = store.getState();
+//     return fetchComponentData(store, renderProps.components, renderProps.params)
+//       .then(() => {
+//         const initialView = renderToString(
+//           <Provider store={store}>
+//             <IntlWrapper>
+//               <RouterContext {...renderProps} />
+//             </IntlWrapper>
+//           </Provider>
+//         );
+//         const finalState = store.getState();
 
-        res
-          .set('Content-Type', 'text/html')
-          .status(200)
-          .end(renderFullPage(initialView, finalState));
-      })
-      .catch((error) => next(error));
-  });
-});
+//         res
+//           .set('Content-Type', 'text/html')
+//           .status(200)
+//           .end(renderFullPage(initialView, finalState));
+//       })
+//       .catch((error) => next(error));
+//   });
+// });
 
 // start app
 app.listen(serverConfig.port, (error) => {
