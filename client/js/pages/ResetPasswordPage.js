7c2333e96f8917cf3c2ab7f722f9a0b3a8c16674
sumitkumar0001 <sumit.kumar@ongraph.com>
2016-10-27 14:10:00 +0530

    reset password changes

--- client/js/pages/ResetPasswordPage.js
+++ client/js/pages/ResetPasswordPage.js
@@ -5,18 +5,24 @@ import { ResetPasswordForm } from 'react-stormpath';
 
 export default class ResetPasswordPage extends React.Component {
 
+  onFormSubmit(e, next) {
+    var data = e.data;
+    data.username = data.username.toLowerCase();
+    next(null, data);
+  }
+
   render() {
     var spToken = this.props.location.query.sptoken;
     return (
       <div className="col-lg-4 margin_auto mtop30" id="" >
-        <form id="resetPasswordForm" method="post" action="/passwordReset" novalidate="novalidate"  className="login_form reset_form">
+        <form id="resetPasswordForm" method="get" action="/passwordReset" onSubmit={this.onFormSubmit.bind(this)} novalidate="novalidate"  className="login_form reset_form">
           <div class="form-group">
             <label for="password" path="password">Password</label>
-            <input type="password" path="password" className="form-control" id="password" name="password"  required={ true } />
+            <input type="password" path="password" className="form-control" id="password" name="password" />
           </div>
           <div class="form-group">
             <label for="confirmedPassword" path="confirmedPassword">Password (confirm)</label>
-            <input type="password" className="form-control" id="confirmedPassword" name="confirmedPassword"  required={ true } />
+            <input type="password" className="form-control" id="confirmedPassword" name="confirmedPassword" />
           </div>
           <input path="sptoken" type="hidden" id="sptoken" name="sptoken" value={spToken} />
           <input name="csrfToken" type="hidden" value="" />
