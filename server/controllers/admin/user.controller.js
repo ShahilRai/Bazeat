add4174eb4bd3dcfe50ba8f77e68bb2db56abe38
Wasim Akhtar <wasim.akhtar@ongraph.com>
2016-11-14 13:11:11 +0530

    user managemenet for admin done

--- server/controllers/admin/user.controller.js
+++ server/controllers/admin/user.controller.js
@@ -3,14 +3,31 @@ import User from '../../models/user';
 
 
 export function allUsers(req, res) {
-  console.log('users')
-  console.log('users')
-  console.log('users')
-  console.log('users')
   User.find().sort('-dateAdded').exec((err, users) => {
     if (err) {
       res.status(500).send(err);
     }
     res.json({ users });
   });
+}
+
+
+export function destroyUser(req, res) {
+  User.findOne({ email: req.params.email }).exec((err, user) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+
+    user.remove(() => {
+      res.status(200).end();
+    });
+  });
+}
+
+
+
+export function editUser(req, res) {
+  User.update({ email: req.body.email }, req.body, function(err, user) {
+    res.json({ user });
+  })
 }
--- server/routes/admin/users.routes.js
+++ server/routes/admin/users.routes.js
@@ -2,7 +2,11 @@ import { Router } from 'express';
 import * as UserController from '../../controllers/admin/user.controller';
 const router = new Router();
 
-router.route('/abc').get(UserController.allUsers);
-console.log('routes')
+router.route('/allusers').get(UserController.allUsers);
+
+
+router.route('/edit').post(UserController.editUser);
+
+router.route('/user/destroy').delete(UserController.destroyUser);
 
 export default router;
