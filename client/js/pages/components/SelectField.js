0625425421f9c298c2ce66115ffde06cce9a6938
Wasim akhtar <wasim.akhtar@ongraph.com>
2016-11-17 12:29:26 +0530

    edit profile changes

--- client/js/pages/components/SelectField.js
+++ client/js/pages/components/SelectField.js
@@ -1,27 +1,28 @@
 import React from 'react';
-var SimpleSelect = require("react-selectize");
-export default class SelectField extends React.Component {
+export default class SelectField extends React.Component{
+     constructor(props) {
+      super(props);
+      this.state={
+        value: this.props.value
+      };
+      this.handleChange=this.handleChange.bind(this);
 
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      value: this.props.value
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-    
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-    if (this.props.onChange){
+     }
+     handleChange(event){
+         this.setState({value: event.target.value});
+         if (this.props.onChange){
             this.props.onChange(event);
         }
-  }
-
-  render() {
-    return(
-      <div className="col-md-8 col-xs-12">
-        <SimpleSelect  placeholder = "Select a fruit"></SimpleSelect>
-      </div>  
-    );
-  }      
+     }
+     render(){
+        return(
+           <div>
+               <select id={this.props.name} name ={this.props.name} onChange={this.handleChange} value={this.state.value}>
+                  <option value="Select One">Select One</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+               </select>
+           </div>
+        );
+     }
 }
