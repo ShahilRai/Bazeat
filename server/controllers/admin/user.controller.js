ffdde344161e6294aa4d88ec72eb9e3993e14694
wasim akhtar <wasim.akhtar@ongraph.com>
2016-11-25 12:33:29 +0530

    fixes done

--- server/controllers/admin/user.controller.js
+++ server/controllers/admin/user.controller.js
@@ -5,18 +5,22 @@ import User from '../../models/user';
 export function getUsers(req, res) {
   console.log('req')
   console.log(req)
-  console.log(req.query._end)
+  let end = parseInt(req.query._end, 10);
+  let start = parseInt(req.query._start, 10);
+  let sort = req.query._sort;
+  console.log(sort)
+  // let end = parseInt(req.query._end, 10);
   console.log(req.query._start)
   console.log(req.query.id)
   console.log('req.query.id')
-  User.find().limit(req.query._end).skip(req.query._start).sort(-req.query.id).exec((err, users) => {
+  User.find().sort('-sort').limit(end).skip(start).exec((err, users) => {
     if (err) {
       res.status(500).send(err);
     }
     // console.log(User.count)
-    res.setHeader('X-Total-Count', 10);
-    res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
-    res.setHeader('X-Content-Type-Options', 'npsniff');
+    // res.setHeader('X-Total-Count', 10);
+    // res.setHeader('Access-Control-Expose-Headers', 'X-Total-Count');
+    // res.setHeader('X-Content-Type-Options', 'npsniff');
     res.json( users );
   });
 }
