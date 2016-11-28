28f19a6502171ecd68d24eacd2ab1698ad2f9d10
wasim akhtar <wasim.akhtar@ongraph.com>
2016-11-28 14:17:38 +0530

    redirect to pages

--- client/js/pages/Product/ReactSlider.js
+++ client/js/pages/Product/ReactSlider.js
@@ -8,6 +8,10 @@ var fieldValues = {}
 
 export default class ReactSlider extends React.Component {
 
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
   constructor(props) {
     super(props);
     this.state = {
--- client/js/pages/admin/AdminRegisterModal.js
+++ client/js/pages/admin/AdminRegisterModal.js
@@ -1,6 +1,10 @@
 import React from 'react';
 export default class AdminRegisterModal extends React.Component {
 
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  }
+
   constructor(props, context) {
     super(props, context);
       this.state = {
@@ -28,6 +32,7 @@ export default class AdminRegisterModal extends React.Component {
           self.setState({
             registered : true
           })
+        self.context.router.push('/admin-dashboard');
       }).fail(function() {
           console.log('failed to register');
     });
