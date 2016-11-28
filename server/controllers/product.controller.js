e0f2e4fdf59f34b14755a2f92389309980a2969e
Wasim Akhtar <wasim.akhtar@ongraph.com>
2016-11-28 14:52:00 +0530

    product list send with alleregn and ingredients

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -6,7 +6,7 @@ import Ingredient from '../models/ingredient';
 import cuid from 'cuid';
 
 export function addProduct(req, res) {
-  console.log(req.body)
+  console.log(req.session)
   User.findOne({ email: req.body.fieldValues.email }).exec((error, user) => {
     const newProduct = new Product(req.body.fieldValues);
     newProduct.cuid = cuid();
@@ -130,9 +130,14 @@ export function getDetails(req, res){
 
 export function getUserProducts(req, res) {
   User.findOne({ email: req.params.email }).populate('products').exec((err, user) => {
-    if (err){
-      res.status(500).send(err);
-    }
-      res.json({producer: user});
+    user.products.forEach(function(item, index){
+      Product.findOne({ cuid: item.cuid }).populate('allergens').populate('ingredients').exec((err, product) =>{
+        user.products[index].allergens = product.allergens;
+        user.products[index].ingredients = product.ingredients
+        if(user.products.length == index+1){
+          res.json({producer: user});
+        }
+      })
+    });
   });
 }
