8e8ee560ec3c3ad6a67148a602c2d93576b3108e
Wasim akhtar <wasim.akhtar@ongraph.com>
2016-11-29 15:38:40 +0530

    editproduct icon change

--- client/js/pages/admin/AdminPanel.js
+++ client/js/pages/admin/AdminPanel.js
@@ -5,16 +5,21 @@ import { ProductList, ProductEdit } from './ProductList';
 import { convertRESTRequestToHTTP, convertHTTPResponseToREST, apiUrl } from './simple';
 import { Delete } from 'admin-on-rest/lib/mui';
 import UserIcon from 'material-ui/svg-icons/social/group';
+import ProductIcon from 'material-ui/svg-icons/action/book';
 export default class AdminPanel extends React.Component{
   constructor(props){
     super(props);
   }
   render(){
+    console.log("Delete--")
+    console.log(Delete)
     return(
       <Admin title="Admin Dashboard" restClient={jsonServerRestClient("http://localhost:3000/admin")} >
       <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete} icon={UserIcon} />
-      <Resource name="products" list={ProductList} edit={UserEdit} remove={Delete} icon={UserIcon} />
+      <Resource name="products" list={ProductList} edit={ProductEdit} remove={Delete} icon={ProductIcon} />
       </Admin>
     );
+
+
   }
 }
