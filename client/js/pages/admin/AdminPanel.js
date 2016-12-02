37e13777586f04375306432a9fa2ab97a852c76b
Wasim akhtar <wasim.akhtar@ongraph.com>
2016-12-02 19:34:05 +0530

    change in user creation

--- client/js/pages/admin/AdminPanel.js
+++ client/js/pages/admin/AdminPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { jsonServerRestClient, Admin, Resource } from 'admin-on-rest';
 import { UserList, UserCreate, UserEdit } from './Users';
-import { ProductList, ProductEdit } from './ProductList';
+import { ProductList, ProductEdit, ProductCreate } from './ProductList';
 import { Delete } from 'admin-on-rest/lib/mui';
 import UserIcon from 'material-ui/svg-icons/social/group';
 import ProductIcon from 'material-ui/svg-icons/action/book';
@@ -13,7 +13,7 @@ export default class AdminPanel extends React.Component{
     return(
       <Admin title="Admin Dashboard" restClient={jsonServerRestClient("http://localhost:3000/admin")} >
       <Resource name="users" list={UserList} edit={UserEdit} create={UserCreate} remove={Delete} icon={UserIcon} />
-      <Resource name="products" list={ProductList} edit={ProductEdit} remove={Delete} icon={ProductIcon} />
+      <Resource name="products" list={ProductList} edit={ProductEdit} create={ProductCreate} remove={Delete} icon={ProductIcon} />
       </Admin>
     );
 
