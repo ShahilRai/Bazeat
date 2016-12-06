ecd704c4d1f19b45c8ba51d3f295d5a0b667c2dd
wasim akhtar <wasim.akhtar@ongraph.com>
2016-12-06 15:59:35 +0530

    code refactor and addproduct issue solve

--- client/js/pages/AddProduct/AddnewProducts.js
+++ client/js/pages/AddProduct/AddnewProducts.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ProductList from './ProductList';
-import ReactSlider from '../Product/ReactSlider';
+
 
 export default class AddnewProducts extends React.Component {
   constructor(props, context) {
@@ -45,19 +45,6 @@ export default class AddnewProducts extends React.Component {
     }
     return(
       <div>
-        <div className="wall-column">
-          <div className="grid_single_item no_brdr add_item_pduct">
-            <div className="add_prod_div">
-              <a href="#" data-toggle="modal" data-target="#add">
-                 <img src="images/add_prod.png" />
-                  <div className="grid_tile_desc">
-                   <h3>Add new <br/> product</h3>
-                  </div>
-              </a>
-            </div>
-          </div>
-        </div>
-        <ReactSlider id="add" />
         {allProducts}
       </div>
     )
