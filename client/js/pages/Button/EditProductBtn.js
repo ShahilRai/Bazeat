2f4258e2a00a3a1264bc7492f0a068c6d2c5dc50
wasim akhtar <wasim.akhtar@ongraph.com>
2016-12-08 18:44:55 +0530

    edit button add product

--- client/js/pages/Button/EditProductBtn.js
+++ client/js/pages/Button/EditProductBtn.js
@@ -2,8 +2,8 @@ import React from 'react';
 export default class EditProductBtn extends React.Component {
 
   render(){
-	return(
-		<a href="#" data-toggle="modal" data-target={"#" + this.props.index} className="buy_btn disable_btn" onClick={this.props.handlerForEdit}>Edit</a>
-	)
+	 return(
+		  <a href="#" data-toggle="modal" data-target={"#" + this.props.index} className="buy_btn disable_btn" onClick={this.props.handlerForEdit}>Edit</a>
+	 )
   }
 }
