6449ee38f7f598ebe3679765cb5ca323304effec
Wasim akhtar <wasim.akhtar@ongraph.com>
2016-12-13 12:13:28 +0530

    update password

--- client/js/pages/UserSetting/TakeABreak.js
+++ client/js/pages/UserSetting/TakeABreak.js
@@ -4,12 +4,14 @@ export default class TakeABreak extends React.Component {
 
   render() {
 	    return (
-	        <div className="edit_prfile_detail_form">
-				<h3>Take a break</h3>
-			    <div className="end_form">
-					<p className="leavn_txt">Even producers need a break sometimes. Are you planning a vacation. Pleas let your fellow Bazeaters know that you are taking a break and when you intend on sharing your crafts-manship again.</p>
-					<button type="submit" className="btn pull-right redish_btn mtop25">Take a break</button>
-				</div>
+	    <div className="col-lg-9 col-md-8 col-sm-10 col-xs-12 edit_profile_rht_sidebar">
+	      <div className="edit_prfile_detail_form">
+					<h3>Take a break</h3>
+				    <div className="end_form">
+						<p className="leavn_txt">Even producers need a break sometimes. Are you planning a vacation. Pleas let your fellow Bazeaters know that you are taking a break and when you intend on sharing your crafts-manship again.</p>
+						<button type="submit" className="btn pull-right redish_btn mtop25">Take a break</button>
+						</div>
+			  </div>
 			</div>
 	    );
     }
--- client/js/pages/UserSetting/UpdateYourPassword.js
+++ client/js/pages/UserSetting/UpdateYourPassword.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { ResetPasswordForm } from 'react-stormpath';
 
 
 export default class UpdateYourPassword extends React.Component {
-
+  static contextTypes = {
+    authenticated: React.PropTypes.bool,
+    user: React.PropTypes.object
+  };
+ constructor(props, context) {
+    super(props, context);
+    this.state = {
+      email: ''
+    };
+  }
+  onFormSubmit(e, next) {
+    this.setState({
+      email:this.context.user.email
+    })
+    console.log("Form submitted",this.context.user.email);
+     next(null, this.context.user.email);
+  }
   render() {
     return (
+      <div className="col-lg-9 col-md-8 col-sm-10 col-xs-12 edit_profile_rht_sidebar">
         <div className="edit_prfile_detail_form">
           <h3>Update your password</h3>
-          <form className="ptop30">
+          <ResetPasswordForm className="ptop30"  action="/passwordReset" onSubmit={this.onFormSubmit.bind(this)} >
             <div className="passwrd_txt">
               <p className="">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
             </div>
@@ -21,7 +39,7 @@ export default class UpdateYourPassword extends React.Component {
               <div className="form-group row">
                 <label for="example-search-input" className="col-md-4 col-xs-12 col-form-label">New password</label>
                 <div className="col-md-7 col-xs-12">
-                 <input type="text" className="form-control" id="New password" name="Old password" required />
+                 <input type="text" className="form-control" id="New password" name="New password" required />
                 </div>
               </div>
               <div className="form-group row">
@@ -34,8 +52,9 @@ export default class UpdateYourPassword extends React.Component {
             <div className="profile_gry_bot_bar">
               <button type="submit" className="btn pull-right">Update password</button>
             </div>
-          </form>
+          </ResetPasswordForm>
         </div>
+      </div>
     );
   }
 }
