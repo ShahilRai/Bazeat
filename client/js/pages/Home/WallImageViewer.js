09d3df77c8bd9ee50b1f3cb6f11ff209cf08f517
wasim akhtar <wasim.akhtar@ongraph.com>
2016-12-26 10:32:36 +0530

    home page product modal fixed

--- client/js/pages/Home/WallImageViewer.js
+++ client/js/pages/Home/WallImageViewer.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ProductDetails from '../AddProduct/ProductDetails';
+import ReactSlider from '../Product/ReactSlider';
 
 export default class WallImageViewer extends React.Component {
+
+  static contextTypes = {
+    authenticated: React.PropTypes.bool,
+    user: React.PropTypes.object
+  };
 
    constructor() {
       super();
@@ -24,15 +30,16 @@ export default class WallImageViewer extends React.Component {
               <small className="icon_text">Buy</small>
             </a>
           </div>
-          <a href="#" data-target={"#id1" + this.props.index} data-toggle="modal">
-            <img src={this.props.wallImages.photo} />
+          <a href="#" data-target={this.context.authenticated == true && this.context.user.email == (this.props.wallImages ? this.props.wallImages._producer.email : this.props.prodlist._producer.email )? "#" + this.props.index : "#id1" + this.props.index} data-toggle="modal">
+            <img src={this.props.wallImages ? this.props.wallImages.photo : this.props.prodlist.photo} />
             <div className="grid_tile_desc">
-              <h2>{this.props.wallImages.product_name}</h2>
-              <span className="price_tag">kr {this.props.wallImages.price}.00</span>
-              <p>{this.props.wallImages.description}</p>
+              <h2>{this.props.wallImages ? this.props.wallImages.product_name : this.props.prodlist.product_name}</h2>
+              <span className="price_tag">kr {this.props.wallImages ? this.props.wallImages.price : this.props.prodlist.price}.00</span>
+              <p>{this.props.wallImages ? this.props.wallImages.description : this.props.prodlist.description}</p>
             </div>
           </a>
-          <ProductDetails index={this.props.index} dsplyProdDetails={this.props.wallImages}/>
+          <ProductDetails index={this.props.index} dsplyProdDetails={this.props.wallImages ? this.props.wallImages : this.props.prodlist}/>
+          <ReactSlider prod_to_edit={this.props.wallImages ? this.props.wallImages : this.props.prodlist} index={this.props.index}/>
         </div>
       </div>
     );
