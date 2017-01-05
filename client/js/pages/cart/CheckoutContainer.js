8664006fa7a0a90a1ec1014d3fcd3b3eee86c5be
wasim akhtar <wasim.akhtar@ongraph.com>
2017-01-05 13:32:03 +0530

    delivery method selection

--- client/js/pages/cart/CheckoutContainer.js
+++ client/js/pages/cart/CheckoutContainer.js
@@ -10,12 +10,14 @@ export default class CheckoutContainer extends React.Component {
     user: React.PropTypes.object
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      step: 1
+      step: 1,
+      selected_method:''
     }
     this.nextStep = this.nextStep.bind(this)
+    this.methodChange =this.methodChange.bind(this)
   }
 
   nextStep() {
@@ -25,14 +27,20 @@ export default class CheckoutContainer extends React.Component {
     })
   }
 
+  methodChange(selected){
+    this.setState({
+      selected_method: selected
+    });
+  }
+
   checkoutStep() {
     switch (this.state.step) {
       case 1:
         return <ShoppingBag nextStep={this.nextStep} step={this.state.step}/>
       case 2:
-        return <DeliveryType nextStep={this.nextStep} step={this.state.step}/>
+        return <DeliveryType nextStep={this.nextStep} step={this.state.step} deliveryMethodChange={this.methodChange}/>
       case 3:
-        return <ProductPickupDate nextStep={this.nextStep} step={this.state.step}/>
+        return <ProductPickupDate nextStep={this.nextStep} step={this.state.step} method={this.state.selected_method}/>
       case 4:  
         return <OrderConfirmation nextStep={this.nextStep} step={this.state.step}/>
       case 5:          
