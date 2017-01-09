c025ef80cc47324a57b16a33cebaa1af508150c0
Wasim akhtar <wasim.akhtar@ongraph.com>
2017-01-09 17:49:48 +0530

    checkoutstep first implemented

--- client/js/pages/cart/ShoppingBag.js
+++ client/js/pages/cart/ShoppingBag.js
@@ -6,7 +6,8 @@ export default class ShoppingBag extends React.Component {
 
   static contextTypes = {
     authenticated: React.PropTypes.bool,
-    user: React.PropTypes.object
+    user: React.PropTypes.object,
+    router: React.PropTypes.object.isRequired
   };
 
   constructor(props, context) {
@@ -24,24 +25,17 @@ export default class ShoppingBag extends React.Component {
     }
   }
 
-  addItem(e, item) {
-    this.state.items.push(item);
-      this.setState({
-        total_price : item.price + this.state.total_price,
-        total_items : this.state.total_items + 1
-      })
-    this.forceUpdate();
-  }
-
   incrNumItems(e, i) {
   var self = this
   var incrCartProduct = this.state.incrCartProductItems
-  incrCartProduct.product_id = this.state.items[i].id
+  incrCartProduct.product_id = this.state.items[i].product_id
+  incrCartProduct.qty = this.state.items[i].qty + 1
   this.incrCartData(incrCartProduct, self.context.user.email).then((response) => {
     if(response.data) {
-      this.state.items[i].qty += 1 ;
       this.setState({
-        total_price : this.state.total_price + this.state.items[i].price
+        items: response.data.cart.cartitems,
+        item : response.data.cart,
+        total_price : response.data.cart.total_price
       })
     }
   }).catch((err) => {
@@ -49,23 +43,25 @@ export default class ShoppingBag extends React.Component {
     });
   }
 
-   decrNumItems(e, i) {
-     var self = this
-     var incrCartProduct = this.state.incrCartProductItems
-     incrCartProduct.product_id = this.state.items[i].id
+  decrNumItems(e, i) {
+   var self = this
+   if(this.state.items[i].qty > 1){
+    var incrCartProduct = this.state.incrCartProductItems
+    incrCartProduct.product_id = this.state.items[i].product_id
+    incrCartProduct.qty = this.state.items[i].qty - 1
      this.incrCartData(incrCartProduct, self.context.user.email).then((response) => {
-      if(response.data) {
-        if(this.state.items[i].qty > 1){
-          this.state.items[i].qty -= 1 ;
+        if(response.data) {
           this.setState({
-            total_price : this.state.total_price - this.state.items[i].price
+            items: response.data.cart.cartitems,
+            item : response.data.cart,
+            total_price : response.data.cart.total_price
           })
         }
-      }
-    }).catch((err) => {
-      console.log(err);
-    });
-   }
+       }).catch((err) => {
+        console.log(err);
+      });
+    }
+  }
 
   incrCartData(cartArray, emailAddress) {
     return axios.post("/api/carts" , {
@@ -74,26 +70,57 @@ export default class ShoppingBag extends React.Component {
     });
   }
 
-  removeItem(e, i) {
-    var self = this
-    var total_qty = this.state.items[i].qty
-    var price =  self.state.items[i].price
-    var total_price = total_qty * price
-    this.state.items.splice(i, 1)
-    this.setState({
-      total_price : self.state.total_price - total_price,
-      total_items : self.state.total_items - 1
-    })
-  }
-
   removeAllItems(){
-    this.setState({
-     items : [],
-     total_price: 0,
-     total_items : 0
-    })
+    var self = this
+    var emailAddress = self.context.user.email
+    this.emptyBag(emailAddress).then((response) => {
+      if(response.data) {
+        this.setState({
+          items : [],
+          total_price: 0,
+          total_items : 0
+        })
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
+//delete all item from bag
+  emptyBag(emailAddress){
+    return axios.delete("/api/empty/cart?email="+emailAddress);
+  }
+
+  removeSingleitem(i){
+    var self = this
+    var emailAddress = self.context.user.email
+    var cartitem_id = this.state.items[i].id
+    this.removeItem(emailAddress, cartitem_id).then((response) => {
+      if(response.data) {
+        this.setState({
+          items : response.data.doc.cartitems,
+          item : response.data.doc,
+          total_price : response.data.doc.total_price
+        })
+      }
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+// delete single item from bag
+  removeItem(emailAddress, cartitem_id){
+    return axios.delete("/api/remove/cart_items/",{
+      params:{
+        email: emailAddress,
+        cartitem_id: cartitem_id
+      }
+    });
+  }
+
+  gotoHome(){
+    window.location.href = "/"
+  }
 
   componentDidMount(){
     var email = this.context.user.email;
@@ -101,7 +128,8 @@ export default class ShoppingBag extends React.Component {
       if(response.data){
         this.setState({
         item : response.data.cart,
-        items : response.data.cart ? response.data.cart.cartitems : 'null'
+        items : response.data.cart.cartitems,
+        total_price : response.data.cart.total_price
         })
       }
     }).catch((err) =>{
@@ -114,8 +142,11 @@ export default class ShoppingBag extends React.Component {
   }
 
   render() {
-    return (
-      <div className="page_wrapper">
+    var goToShopBtn
+    if(this.state.items.length<1){
+      goToShopBtn = <button type="submit" className="btn pull-left step1_emptybtn goto_shop_btn" onClick={this.gotoHome.bind(this)}>Go to ShopHome</button>
+    }else{
+      goToShopBtn =<div className="page_wrapper">
         <div className="full_width ptop0">
           <div className="chkout_pg chkoutstep1">
             <h3>Shopping bag</h3>
@@ -131,35 +162,41 @@ export default class ShoppingBag extends React.Component {
             </ul>
             <div className="items_list_info" style={{display: 'block'}}>
               <ul>
-              {this.state.items.map((item, i)=>
-                <li key={i}>
-                  <span className="sr_no">
-                    <button className="fa fa-caret-up" onClick={(e) => this.incrNumItems(e, i)}></button>
-                      <input type="text" className="form-control text-center" value={item.qty} data-rule="quantity" type="text" />
-                    <button className="fa fa-caret-down" onClick={(e) => this.decrNumItems(e, i)} ></button>
+                {this.state.items.map((item, i)=>
+                  <li key={i}>
+                    <span className="sr_no">
+                      <button className="fa fa-caret-up" onClick={(e) => this.incrNumItems(e, i)}></button>
+                        <input type="text" className="form-control text-center" value={item.qty} data-rule="quantity" type="text" />
+                      <button className="fa fa-caret-down" onClick={(e) => this.decrNumItems(e, i)} ></button>
+                    </span>
+                    <span className="list_images"><img src={item.product_image} height="50" width="50"/>
+                    <small>{item.product_name}</small>
+                    </span>
+                  <span className="items_price">{"kr " + item.product_amt}</span>
+                  <span className="mva">15%</span>
+                  <span className="items_price">{item.product_total_amt}</span>
+                  <span className="del_bin">
+                    <a href= "javaScript:void(0)"><img src="images/del_bin.png" onClick={this.removeSingleitem.bind(this,i)}/></a>
                   </span>
-                  <span className="list_images">{item.id}
-                  </span>
-                <span className="items_price">{"kr " + item.price}</span>
-                <span className="mva">15%</span>
-                <span className="items_price">kr 35,00</span>
-                <span className="del_bin">
-                  <img src="images/del_bin.png"/>
-                </span>
-                </li>
-              )}
-            </ul>
-              <div className="chkout_step1_footer">
+                  </li>
+                )}
+              </ul>
+              <div className="chkout_step1_footer">total_price
                 <span className="chkout_step1_prod">{"In total " +this.state.items.length +" products"}</span>
-                <span className="chkout_step1_totalprice">{"Total price incl. MVA: kr " + this.state.item.total_price}</span>
+                <span className="chkout_step1_totalprice">{"Total price incl. MVA: kr " + this.state.total_price}</span>
               </div>
             </div>
             <div className="chkout_step1btns">
-              <button type="submit" className="btn pull-left step1_emptybtn">Empty shopping bag</button>
+              <button type="submit" className="btn pull-left step1_emptybtn" onClick={this.removeAllItems.bind(this)}>Empty shopping bag</button>
               <button type="button" className="btn btn-default continue_btn" onClick={this.props.nextStep}>Continue</button>
             </div>
           </div>
         </div>
+      </div>
+    }
+    return (
+      <div className="full_width_container">
+        {goToShopBtn}
       </div>
     );
   }
