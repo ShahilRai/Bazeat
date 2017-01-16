fcb754022de70bb085c66f2eef57e2041bcb2ad8
Wasim Akhtar <wasim.akhtar@ongraph.com>
2017-01-16 12:55:50 +0530

    added Twilio SmS service

--- server/server.js
+++ server/server.js
@@ -396,32 +396,6 @@ app.get('/admin/logouts', logout());
 //   res.send('Welcome back: ' + res.locals.user.email);
 // });
 
-// let client = new twilio.RestClient(process.env.TwilioSid, process.env.TwilioToken);
-
-// client.sms.messages.create({
-//     to:'+917840827410',
-//     from:'+16318134710',
-//     body:'ahoy hoy! Testing Twilio and node.js'
-// }, function(error, message) {
-//     // The HTTP request to Twilio will run asynchronously. This callback
-//     // function will be called when a response is received from Twilio
-//     // The "error" variable will contain error information, if any.
-//     // If the request was successful, this value will be "falsy"
-//     if (!error) {
-//         // The second argument to the callback will contain the information
-//         // sent back by Twilio for the request. In this case, it is the
-//         // information about the text messsage you just sent:
-//         console.log('Success! The SID for this SMS message is:');
-//         console.log(message.sid);
-
-//         console.log('Message sent on:');
-//         console.log(message.dateCreated);
-//     } else {
-//         console.log(error);
-//         console.log('Oops! There was an error.');
-//     }
-// });
-
 // const transport = nodeMailer.createTransport(mandrillTransport({
 //   auth: {
 //     apiKey: process.env.MandrilKey
