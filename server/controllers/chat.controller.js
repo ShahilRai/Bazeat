89ade547e641098f30377351bde1a483d31f330d
Wasim Akhtar <wasim.akhtar@ongraph.com>
2017-01-24 18:57:24 +0530

    updated messages unread messages

--- server/controllers/chat.controller.js
+++ server/controllers/chat.controller.js
@@ -65,7 +65,13 @@ export function getConversation(req, res, next) {
         return next(err);
       }
       if(messages){
-        return res.status(200).json({ conversation: messages });
+        messages.forEach(function(msg, index) {
+           Message.findOneAndUpdate({_id: msg._id}, {$set: {'unread': false}}, {new: true}).exec(function(err, model){
+            if (messages.length == index+1){
+              return res.status(200).json({ conversation: model });
+            }
+           })
+        })
       }
       else{
         return res.status(200).json({ conversation: "There are no messages for this conversation" });
