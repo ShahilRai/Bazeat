7ec7d48c34487a854f39fe865b908b1ba4ffbeb9
Wasim Akhtar <wasim.akhtar@ongraph.com>
2017-01-30 16:41:16 +0530

    changes in carts

--- server/routes/cart.routes.js
+++ server/routes/cart.routes.js
@@ -2,13 +2,10 @@ import { Router } from 'express';
 import * as CartController from '../controllers/cart.controller';
 const router = new Router();
 
-  router.route('/cart/:email').get(CartController.getCart);
+  router.route('/cart/:cuid').get(CartController.getCart);
 
 // Add to cart
   router.route('/carts').post(CartController.createCart);
-
-
-  router.route('/sessioncarts').post(CartController.sessionCart);
 
 // Remove cartitems by _id
   router.route('/remove/cart_items').delete(CartController.removeCartItems);
