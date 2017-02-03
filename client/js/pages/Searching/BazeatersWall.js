2631d094e3573c569acafa811a1e5c13582f86cd
wasim akhtar <wasim.akhtar@ongraph.com>
2017-02-03 12:52:52 +0530

    conflict resolved

--- client/js/pages/Searching/BazeatersWall.js
+++ client/js/pages/Searching/BazeatersWall.js
@@ -10,7 +10,6 @@ export default class BazeatersWall extends React.Component {
           <a href="#" className="search_open">Open</a>
           <div className="bazeater_logo1">
             <span className="pos_rel"><img className="bazeaters_logo" src={this.props.bazeatersData.photo} />
-              <img src="images/revw_icon.png" className="chkd_img" />
             </span>
           </div>
           <h4 ><Link to={"/user/"+this.props.bazeatersData.cuid} className = "font_colr">{this.props.bazeatersData.full_name}</Link></h4>
