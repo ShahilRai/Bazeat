bcc362ea4f27f0db688355f0a3747036b35a87d6
wasim akhtar <wasim.akhtar@ongraph.com>
2017-02-08 18:55:25 +0530

    purchase order calculations

--- client/js/pages/OrderManagement/ReceivedOrder.js
+++ client/js/pages/OrderManagement/ReceivedOrder.js
@@ -223,8 +223,8 @@ export default class ReceivedOrder extends React.Component {
                                   <span>{order.packed_qty} Packed</span>
                                   <span>{order.shipped_qty} Shipped</span>
                                 </td>
-                                <td className="text-right">{order.product_price}</td>
-                                <td className="text-right">{order.product_weight}</td>
+                                <td className="text-right">{order.product_price.toFixed(2)}</td>
+                                <td className="text-right">{order.product_weight.toFixed(2)}</td>
                                 <td className="">
                                   <a href="#" className="red_font"><i className="fa fa-pencil-square-o" aria-hidden="true"></i></a>
                                 </td>
@@ -236,15 +236,15 @@ export default class ReceivedOrder extends React.Component {
                         <div className="order_subtotal order_rcvd_subtot">
                           <div className="inner_order_subtot">
                             <span className="mrht75">Sub total</span>
-                            <span>1250,00</span>
+                            <span>{this.state.orderDetails.net_price? this.state.orderDetails.net_price.toFixed(2): ""}</span>
                           </div>
                           <div className="inner_order_subtot">
                             <span className="mrht75 prht15">Where of MVA (15%)</span>
-                            <span>{this.state.orderDetails.vat}</span>
+                            <span>{this.state.orderDetails.food_vat_value? this.state.orderDetails.food_vat_value.toFixed(2): ""}</span>
                           </div>
                           <div className="inner_order_subtot">
                             <span className="mrht75">Shipment</span>
-                            <span>{this.state.orderDetails.price_with_ship ? this.state.orderDetails.price_with_ship.toFixed(2): ""}</span>
+                            <span>{this.state.orderDetails.shipment_price ? this.state.orderDetails.shipment_price.toFixed(2): ""}</span>
                           </div>
                           <div className="inner_order_subtot">
                             <span className="mrht75 prht15">Where of MVA (25%)</span>
