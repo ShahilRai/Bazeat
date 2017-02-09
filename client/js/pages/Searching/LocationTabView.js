d393906c1b209c630374dffbefcb9f27119ffe63
wasim akhtar <wasim.akhtar@ongraph.com>
2017-02-09 11:58:18 +0530

    default user image

--- client/js/pages/Searching/LocationTabView.js
+++ client/js/pages/Searching/LocationTabView.js
@@ -15,10 +15,14 @@ export default class LocationTabView extends React.Component {
     return newarray1.join(' ');
   }
   render() {
+    var src=this.props.locData.photo
+    if(src==undefined){
+      src=require('../../../images/user_picture.png') 
+    }
     return (
       <div className="search_row1 grey_bg">
         <div className="search_row_wdth">
-          <span className="s_name wdth_11"><img  className="location_logo" src={this.props.locData.photo} /></span>
+          <span className="s_name wdth_11"><img  className="location_logo" src={src} /></span>
           <span className="s_name name_wdth26"><Link to={"/user/"+this.props.locData.cuid} className = "font_colr">{this.upperCase(this.props.locData.full_name)}</Link></span>
           <span className="rvws_wdth star_rating">
             <Rating rating={this.props.locData.avg_rating} displayOnly={true} maxRating={5}  ratingSymbol={"\u2764"} />
