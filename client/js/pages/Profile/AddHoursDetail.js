265c78fb97d838deb874bb0667cee93f5e2926cc
wasim akhtar <wasim.akhtar@ongraph.com>
2017-02-13 16:02:20 +0530

    add hours detail issue fix

--- client/js/pages/Profile/AddHoursDetail.js
+++ client/js/pages/Profile/AddHoursDetail.js
@@ -35,13 +35,9 @@ export default class AddHoursDetail extends React.Component {
         return alert("Please select time" );
       }
       this.state.selectDays = [];
-      this.state.selectDays.push(this.state.days[0].value)
-      this.state.selectDays.push(this.state.days[1].value)
-      this.state.selectDays.push(this.state.days[2].value)
-      this.state.selectDays.push(this.state.days[3].value)
-      this.state.selectDays.push(this.state.days[4].value)
-      this.state.selectDays.push(this.state.days[5].value)
-      this.state.selectDays.push(this.state.days[6].value)
+      for(var i=0;i<7;i++){
+        this.state.selectDays.push(this.state.days[i].value)
+      }
     }
 
     var hoursArray = []
@@ -102,7 +98,6 @@ export default class AddHoursDetail extends React.Component {
 
   _deleteTitmeSlot(_timeSlotId){
     this.removeHours(_timeSlotId).then((response) => {
-      console.log(response.data)
       if(response.data) {
         this.setState({
           items: response.data.timeslot.timeslots
